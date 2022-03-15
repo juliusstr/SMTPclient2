4cb28bf15f0945149f6c335dd8921f18b25ca698
Juliusstr <julius@struwing.dk>
2022-03-15 17:26:03 +0100

    134578

--- src/main/java/SenderMail.java
+++ src/main/java/SenderMail.java
@@ -10,22 +10,10 @@ import java.util.Properties;
 
 public class SenderMail {
     public static void mail(String from, String password, String to, int port, String filepath, String subjekt, String text) {
-
-        //String filepath = "C:/Users/juliu/Pictures/Camera Roll/WIN_20220308_15_19_16_Pro.jpg";
-
-        // Recipient's email ID needs to be mentioned.
-        //String from = "struwingjulius3@gmail.com";
-
-        // Sender's email ID needs to be mentioned
-        //String to = "anlcmail1992@gmail.com";
-
-        // Assuming you are sending email from through gmails smtp
         String host = "smtp.gmail.com";
 
-        // Get system properties
         Properties properties = System.getProperties();
 
-        // Setup mail server
         properties.put("mail.smtp.host", host);
         properties.put("mail.smtp.port", port);
         properties.put("mail.smtp.ssl.enable", "true");
